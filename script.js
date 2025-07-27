52fcf029a3ebfd90ccf89a094c6c457da491b366
Georginio397 <vinst.nftpromoter@gmail.com>
2025-07-27 14:22:17 +0300

    .

--- script.js
+++ script.js
@@ -40,6 +40,32 @@ document.querySelectorAll(".meme-gallery img").forEach((img) => {
   function closeLightbox() {
     document.getElementById("lightbox").style.display = "none";
   }
+
+  
+  function createFallingItem() {
+    const item = document.createElement('div');
+    item.classList.add('falling-item');
+  
+    const size = Math.random() * 80 + 40; // random dimensiune
+    item.style.width = `${size}px`;
+    item.style.height = `${size}px`;
+  
+    item.style.left = `${Math.random() * 100}%`;
+    item.style.animationDuration = `${Math.random() * 5 + 5}s`; // între 5 și 10s
+    item.style.animationDelay = `${Math.random() * 3}s`;
+  
+    document.getElementById('falling-overlay').appendChild(item);
+  
+    // Eliminare după animație
+    setTimeout(() => {
+      item.remove();
+    }, 12000);
+  }
+  
+  // creează continuu elemente la intervale
+  setInterval(createFallingItem, 600);
+  
+  
   
 
 window.onload = () => {
